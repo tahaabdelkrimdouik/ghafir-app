588a69ca20ac46bedf5663a32d7fab56376d76bb
douiktahaabdelkrim <abdelkrim@owlstack.agency>
2026-01-10 14:42:43 +0100

    fix build

--- src/components/OneSignalInit.tsx
+++ src/components/OneSignalInit.tsx
@@ -20,6 +20,23 @@ export default function OneSignalInit() {
         
         notifyButton: {
           enable: true,
+          prenotify: true,
+          showCredit: false,
+          text: {
+            'tip.state.unsubscribed': 'Subscribe to notifications',
+            'tip.state.subscribed': "You're subscribed to notifications",
+            'tip.state.blocked': "You've blocked notifications",
+            'message.prenotify': 'Click to subscribe to notifications',
+            'message.action.subscribed': 'Thanks for subscribing!',
+            'message.action.resubscribed': 'You\'re subscribed to notifications',
+            'message.action.subscribing': 'Subscribing...',
+            'message.action.unsubscribed': 'You won\'t receive notifications again',
+            'dialog.main.title': 'Manage Site Notifications',
+            'dialog.main.button.subscribe': 'SUBSCRIBE',
+            'dialog.main.button.unsubscribe': 'UNSUBSCRIBE',
+            'dialog.blocked.title': 'Unblock Notifications',
+            'dialog.blocked.message': 'Follow these instructions to allow notifications:'
+          }
         },
         // 👇 Keep this true while testing on your computer
         allowLocalhostAsSecureOrigin: true, 
